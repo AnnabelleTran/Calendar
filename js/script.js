9c456a6fe18156827420992437bb96cdac74ef46
Annabelle Tran <annabelletran@hotmail.com>
2022-04-19 14:55:50 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,95 @@
+var today = new Date();
+function findDate(){
+var date = today.getDate();
+//highlight the current day
+        if(date == 1){
+            document.getElementById("d1").style.backgroundColor = "darkred";
+        }
+        if(date == 2){
+            document.getElementById("d2").style.backgroundColor = "darkred";
+        }
+        if(date == 3){
+            document.getElementById("d3").style.backgroundColor = "darkred";
+        }
+        if(date == 4){
+            document.getElementById("d4").style.backgroundColor = "darkred";
+        }
+        if(date == 5){
+            document.getElementById("d5").style.backgroundColor = "darkred";
+        }
+        if(date == 6){
+            document.getElementById("d6").style.backgroundColor = "darkred";
+        }
+        if(date == 7){
+            document.getElementById("d7").style.backgroundColor = "darkred";
+        }
+        if(date == 8){
+            document.getElementById("d8").style.backgroundColor = "darkred";
+        }
+        if(date == 9){
+            document.getElementById("d9").style.backgroundColor = "darkred";
+        }
+        if(date == 10){
+            document.getElementById("d10").style.backgroundColor = "darkred";
+        }
+        if(date == 11){
+            document.getElementById("d11").style.backgroundColor = "darkred";
+        }
+        if(date == 12){
+            document.getElementById("d12").style.backgroundColor = "darkred";
+        }
+        if(date == 13){
+            document.getElementById("d13").style.backgroundColor = "darkred";
+        }
+        if(date == 14){
+            document.getElementById("d14").style.backgroundColor = "darkred";
+        }
+        if(date == 15){
+            document.getElementById("d15").style.backgroundColor = "darkred";
+        }
+        if(date == 16){
+            document.getElementById("d16").style.backgroundColor = "darkred";
+        }
+        if(date == 17){
+            document.getElementById("d17").style.backgroundColor = "darkred";
+        }
+        if(date == 18){
+            document.getElementById("d18").style.backgroundColor = "darkred";
+        }
+        if(date == 19){
+            document.getElementById("d19").style.backgroundColor = "darkred";
+        }
+        if(date == 20){
+            document.getElementById("d20").style.backgroundColor = "darkred";
+        }
+        if(date == 21){
+            document.getElementById("d21").style.backgroundColor = "darkred";
+        }
+        if(date == 22){
+            document.getElementById("d22").style.backgroundColor = "darkred";
+        }
+        if(date == 23){
+            document.getElementById("d23").style.backgroundColor = "darkred";
+        }
+        if(date == 24){
+            document.getElementById("d24").style.backgroundColor = "darkred";
+        }
+        if(date == 25){
+            document.getElementById("d25").style.backgroundColor = "darkred";
+        }
+        if(date == 26){
+            document.getElementById("d26").style.backgroundColor = "darkred";
+        }
+        if(date == 27){
+            document.getElementById("d27").style.backgroundColor = "darkred";
+        }
+        if(date == 28){
+            document.getElementById("d28").style.backgroundColor = "darkred";
+        }
+        if(date == 29){
+            document.getElementById("d29").style.backgroundColor = "darkred";
+        }
+        if(date == 30){
+            document.getElementById("d30").style.backgroundColor = "darkred";
+        }
+    }
